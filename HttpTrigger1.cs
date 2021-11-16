a1d9bdac33bfd8fc259414cc2c30a00ef11b0a6a
Lyon Till <lyon.till@microsoft.com>
2021-11-16 11:05:02 +0000

    Update HttpTrigger1.cs

--- HttpTrigger1.cs
+++ HttpTrigger1.cs
@@ -59,9 +59,6 @@ namespace Microsoft.AppInnovation.Budgets
             }
 
             var response = req.CreateResponse(HttpStatusCode.OK);
-            //response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-            //response.WriteString("Welcome to Azure Functions!");
-
             return response;
         }
 
