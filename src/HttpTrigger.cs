baa916528cb10bf2ea22e3369f5375ba9947bfb4
Lyon Till <lyon.till@microsoft.com>
2021-11-29 16:40:15 +0000

    Update src/

--- src/HttpTrigger.cs
+++ src/HttpTrigger.cs
@@ -25,14 +25,12 @@ namespace Microsoft.AppInnovation.Budgets
             _logger = loggerFactory.CreateLogger<HttpTrigger>();
         }
 
-        private BudgetAlert alert;
-
         [Function("HttpTrigger")]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            HttpResponseData response;
+            BudgetAlert alert;
 
             #region Parse
             try
@@ -42,28 +40,24 @@ namespace Microsoft.AppInnovation.Budgets
             }
             catch
             {
-                // TODO: Custom error logging
-                // TODO: JSON Response message
-                response = req.CreateResponse(HttpStatusCode.InternalServerError);
+                // TODO: Log message (parsing failed)
+                return req.CreateResponse(HttpStatusCode.InternalServerError);
             }
             #endregion
+
+            string accessToken;
 
             #region Authenticate
             try
             {
                 _logger.LogInformation("Authenticating with identity endpoints.");
-                var accessToken = GetAccessToken(_logger);
-                _logger.LogInformation($"Token: {accessToken}");
-
-                response = req.CreateResponse(HttpStatusCode.OK);
+                accessToken = GetAccessToken();
             }
             catch (Exception e)
             {
-                // TODO: Custom error logging
-                // TODO: JSON Response message
+                // TODO: Log message (invalid credentials)
                 _logger.LogError($"Exception thrown during authentication process (Reason='{e.Message}')");
-                // TODO: Return error response (invalid credentials)
-                response = req.CreateResponse(HttpStatusCode.InternalServerError);
+                return req.CreateResponse(HttpStatusCode.InternalServerError);
             }
             #endregion
 
@@ -71,23 +65,21 @@ namespace Microsoft.AppInnovation.Budgets
             try
             {
                 _logger.LogInformation("Checking Azure subscription exclusion tags.");
-                CheckSubscriptionTags(_logger, alert.Data.SubscriptionId);
+                CheckSubscriptionTags(accessToken, alert.Data.SubscriptionId);
 
                 _logger.LogInformation("Updating Azure subscription state.");
-                DisableSubscription(_logger, alert.Data.SubscriptionId);
+                DisableSubscription(accessToken, alert.Data.SubscriptionId);
             }
             catch (Exception e)
             {
-                // TODO: Custom error logging
-                // TODO: JSON Response message
+                // TODO: Log message (subscription not found)
+                // TODO: Log message (insufficient permissions)
                 _logger.LogError($"Exception thrown during subscription process (Reason='{e.Message}')");
-                // TODO: Return error response (subscription not found)
-                // TODO: Return error response (insufficient permissions)
-                response = req.CreateResponse(HttpStatusCode.InternalServerError);
+                return req.CreateResponse(HttpStatusCode.InternalServerError);
             }
             #endregion
 
-            return response;
+            return req.CreateResponse(HttpStatusCode.OK);
         }
 
         private BudgetAlert ParseHttpRequest(HttpRequestData req)
@@ -105,50 +97,52 @@ namespace Microsoft.AppInnovation.Budgets
             return JsonSerializer.Deserialize<BudgetAlert>(body, options);
         }
 
-        private string GetAccessToken(ILogger logger)
+        private string GetAccessToken()
         {
             AccessToken token;
+            var scopes = new string[] { "https://management.azure.com/.default" };
 
             if (Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") == "Development")
             {
-                logger.LogDebug("Running in development mode.");
+                // TODO: Validate local.settings.json values are not null
+                _logger.LogDebug("Running in development mode.");
                 var clientId = Environment.GetEnvironmentVariable("AZURE_CLIENT_ID");
                 var clientSecret = Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET");
                 var tenantId = Environment.GetEnvironmentVariable("AZURE_TENANT_ID");
 
-                logger.LogDebug("Authenticating client secret credential.");
+                _logger.LogDebug("Authenticating client secret credential.");
                 var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
 
-                logger.LogDebug("Retrieiving access token.");
-                token = credential.GetToken(new TokenRequestContext());
+                _logger.LogDebug("Retrieiving access token.");
+                token = credential.GetToken(new TokenRequestContext(scopes));
             }
             else
             {
-                logger.LogDebug("Running in production mode.");
+                _logger.LogDebug("Running in production mode.");
                 var clientId = Environment.GetEnvironmentVariable("AZURE_MANAGED_IDENTITY");
 
-                logger.LogDebug("Authenticating managed identity credential.");
+                _logger.LogDebug("Authenticating managed identity credential.");
                 var credential = new ManagedIdentityCredential(clientId);
 
-                logger.LogDebug("Retrieving access token.");
-                token = credential.GetToken(new TokenRequestContext());
+                _logger.LogDebug("Retrieving access token.");
+                token = credential.GetToken(new TokenRequestContext(scopes));
             }
 
             return token.Token;
         }
 
-        private void CheckSubscriptionTags(ILogger logger, string SubscriptionId)
+        private void CheckSubscriptionTags(string accessToken, string SubscriptionId)
         {
-            logger.LogDebug("Getting subscription tags.");
+            _logger.LogDebug("Getting subscription tags.");
             // TODO: Add HTTP call
         }
 
-        private void DisableSubscription(ILogger logger, string SubscriptionId)
+        private void DisableSubscription(string accessToken, string SubscriptionId)
         {
             // TODO: Check Subscription state
             // TODO: Add HTTP call
 
-            logger.LogDebug("Setting subscription to disable state.");
+            _logger.LogDebug("Setting subscription to disable state.");
             // TODO: Add HTTP call
         }
     }
